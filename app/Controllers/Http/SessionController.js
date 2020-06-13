169de6a26635280fae005dc2cb3b6a2ba35088d7
Rogério Nascimento <rogerionascimento.dev@outlook.com.br>
2020-06-13 02:07:41 -0300

    ...

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -3,9 +3,9 @@
 const User = use('App/Models/User')
 
 class SessionController {
-    async store({request, response, auth}){
-
+    async store({request, response, auth}){        
         const {email, password} = request.all()           
+        console.log('chegou aqui',email, password);
         const token = await auth.attempt(email, password)
         const user = await User.findBy('email', email);        
         return {token, user}                
--- app/Controllers/Http/UserFacebookController.js
+++ app/Controllers/Http/UserFacebookController.js
@@ -3,7 +3,7 @@ const User = use('App/Models/User')
 
 class UserFacebookController {
     async store({request}){
-        const data = request.only(['name','logged_facebook','email','password'])
+        const data = request.only(['name','logged_facebook','email','password'])        
         const user = await User.findOrCreate({email:data.email} ,{...data})
         return user 
     }
